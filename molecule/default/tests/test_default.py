86e07d80235e32a817b526843f558fa6f7227832
frite <mmavrofides@gmail.com>
2019-11-25 21:46:50 +0200

    Added altdns test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,9 +7,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_altdns(host):
+    ''' Test that altdns and the mode is proper.'''
+    f = host.file('/usr/local/bin/altdns')
 
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+    assert oct(f.mode) == '0o755'
